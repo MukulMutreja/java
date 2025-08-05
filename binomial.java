6d9952a40a2974aef8b02548683535e76a087ef9
MukulMutreja <mukul.mutreja348@gmail.com>
2025-08-05 23:55:35 +0530

    Add files via upload
    Successfully created the code to calculate the binomial coefficient

---
+++ binomial.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class binomial {
+    public static int factorial (int n) {
+        int fact = 1 ;
+        for(int i = 1 ; i<= n ; i++) {
+            fact*=i;
+        }
+        return fact;
+    }
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+        System.out.println("Enter the value of r : ");
+        int r = sc.nextInt();
+        int factorial_num =  factorial(n) /( factorial(r) * factorial(n-r));
+        System.out.println("Factorial of the number : "+factorial_num);
+    }
+}
